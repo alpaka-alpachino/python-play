a550a241a5f0fd9f584ba85fe29575e1953ba1f1
Kateryna Kostiukevych <okkerri@gmail.com>
2023-12-07 16:11:35 +0200

    refactored code

--- poc_1/main.py
+++ poc_1/main.py
@@ -1,23 +1,16 @@
-import logging
-import concurrent.futures
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 from fastapi.responses import StreamingResponse
 import io
 import uvicorn
-import asyncio
 from PIL import Image, ImageDraw
 
 app = FastAPI()
+watermark = Image.new("RGBA", (500, 100), (255, 255, 255, 255))
 
 
-def process_image(file):
+async def process_image(file):
     # Open the file as an image
     input_image = Image.open(file.file)
-
-    # Create a watermark image
-    watermark = Image.new("RGBA", (500, 100), (255, 255, 255, 255))
-    draw = ImageDraw.Draw(watermark)
-    draw.text((10, 10), "watermark", fill="black", font_size=80)
 
     # Composite the watermark onto the input image
     input_image.paste(watermark, (0, 0), watermark)
@@ -32,13 +25,10 @@ def process_image(file):
 
 @app.post("/watermark/")
 async def set_watermark(file: UploadFile):
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-        result_image = await asyncio.to_thread(process_image, file)
+    result_image = await process_image(file)
 
     return StreamingResponse(io.BytesIO(result_image), media_type="image/jpeg")
 
 
 if __name__ == '__main__':
-    # uvicorn main:app --host 0.0.0.0 --port 8080 --reload --workers 2
-    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True, workers=8)
